d044dcdeb7927ac02c7ba5f42a6f850835d6390d
Otiliano Junior <otiliano.rodrigues@veri.com.br>
2023-10-26 15:21:55 -0300

    feat: ataulizacao de fotos arquivo de condfiguracao and classe principal

--- src/gate_face_recognition/GateSecuritySystem.py
+++ src/gate_face_recognition/GateSecuritySystem.py
@@ -12,13 +12,16 @@ class GateSecuritySystem:
         self.FOTOS_PORTAO = None
         self.ARQUIVO_CONFIGURACAO = 'config/configuracao.json'
         self.individuos_registrados = {}
+        self.configuracao = None
+        self.FOTOS_ALUNOS = None
+        self.FOTOS_SUSPEITOS = None
 
     def load_config(self):
         try:
             json_util = JsonUtil(self.ARQUIVO_CONFIGURACAO)
             preparado = json_util.load_file()
-            configuracao = json_util.data
-            return preparado, configuracao
+            self.configuracao = json_util.data
+            return preparado, self.configuracao
         except Exception as ex:
             raise Exception('Erro: load config', ex)
 
@@ -30,6 +33,33 @@ class GateSecuritySystem:
             return self.FOTOS_PORTAO
         except Exception as ex:
             raise Exception('Erro: Load Fotos Gate', ex)
+
+    def load_fotos_alunos(self):
+        try:
+            dir_fotos = 'assets/fotos/alunos'
+            fotos_util = FotosUtil(dir_fotos)
+            self.FOTOS_ALUNOS = fotos_util.carregar_fotos()
+            return self.FOTOS_ALUNOS
+        except Exception as ex:
+            raise Exception('Erro: Load Fotos Alunos', ex)
+
+    def load_fotos_servidores(self):
+        try:
+            dir_fotos = 'assets/fotos/servidores'
+            fotos_util = FotosUtil(dir_fotos)
+            self.FOTOS_SERVIDORES = fotos_util.carregar_fotos()
+            return self.FOTOS_SERVIDORES
+        except Exception as ex:
+            raise Exception('Erro: Load Fotos Servidores', ex)
+
+    def load_fotos_suspeitos(self):
+        try:
+            dir_fotos = 'assets/fotos/suspeitos'
+            fotos_util = FotosUtil(dir_fotos)
+            self.FOTOS_SUSPEITOS = fotos_util.carregar_fotos()
+            return self.FOTOS_SUSPEITOS
+        except Exception as ex:
+            raise Exception('Erro: Load Fotos Suspeitos', ex)
 
     def simular_entrada(self):
         try:
@@ -54,40 +84,82 @@ class GateSecuritySystem:
         except Exception as ex:
             raise Exception('Erro: INdividuo Reconhecido Anteriormente', ex)
 
-    def reconhecer_individuos(self, visitantes, configuracao):
+    def reconhecer_alunos(self, visitante):
         try:
-            print('Realizando reconhecimento de pacientes...')
-            fotos_visitantes = reconhecedor.load_image_file(visitantes['foto'])
-            caracteristicas_visitantes = reconhecedor.face_encodings(fotos_visitantes)
+            foto_visitante = reconhecedor.load_image_file(visitante["foto"])
+            caracteristicas_visitante = reconhecedor.face_encodings(foto_visitante)
 
-            pacientes_encontrados = []
-            for individuo in configuracao['pacientes']:
-                if self.individuo_reconhecido_anteriormente(individuo):
-                    print(f'Paciente {individuo["nome"]}, já foi reconhecido anteriormente')
-                else:
-                    fotos = individuo['fotos']
-                    total_reconhecimentos = 0
+            reconhecidos_alunos = []
 
-                    for foto in fotos:
-                        foto = reconhecedor.load_image_file(foto)
-                        caracteristicas = reconhecedor.face_encodings(foto)[0]
+            for aluno in self.FOTOS_ALUNOS:
+                reconhecido, confianca = self.reconhecer_individual(
+                    caracteristicas_visitante, aluno["fotos"])
+                if reconhecido:
+                    reconhecidos_alunos.append(aluno)
 
-                        reconhecimentos = reconhecedor.compare_faces(
-                            caracteristicas_visitantes, caracteristicas
-                        )
+            return reconhecidos_alunos
 
-                        if True in reconhecimentos:
-                            total_reconhecimentos += 1
-
-                    if total_reconhecimentos / len(fotos) >= 0.6:
-                        pacientes_encontrados.append(reconhecedor)
-
-            return (len(pacientes_encontrados) > 0), pacientes_encontrados
         except Exception as ex:
-            raise Exception('Erro: Reconhecer Pacientes', ex)
+            raise Exception('Erro: Reconhecer Alunos', ex)
+
+    def reconhecer_servidores(self, visitante):
+        try:
+            foto_visitante = reconhecedor.load_image_file(visitante["foto"])
+            caracteristicas_visitante = reconhecedor.face_encodings(foto_visitante)
+
+            reconhecidos_servidores = []
+
+            for servidor in self.FOTOS_SERVIDORES:
+                reconhecido, confianca = self.reconhecer_individual(
+                    caracteristicas_visitante, servidor["fotos"])
+                if reconhecido:
+                    reconhecidos_servidores.append(servidor)
+
+            return reconhecidos_servidores
+
+        except Exception as ex:
+            raise Exception('Erro: Reconhecer Servidores', ex)
+
+    def reconhecer_suspeitos(self, visitante):
+        try:
+
+            foto_visitante = reconhecedor.load_image_file(visitante["foto"])
+            caracteristicas_visitante = reconhecedor.face_encodings(foto_visitante)
+
+            reconhecidos_suspeitos = []
+
+            for suspeito in self.FOTOS_SUSPEITOS:
+                reconhecido, confianca = self.reconhecer_individual(
+                    caracteristicas_visitante, suspeito["fotos"])
+                if reconhecido:
+                    reconhecidos_suspeitos.append(suspeito)
+
+            return reconhecidos_suspeitos
+
+        except Exception as ex:
+            raise Exception('Erro: Reconhecer Suspeitos', ex)
+
+    def reconhecer_individual(self, caracteristicas_visitante, fotos_individuo):
+        total_de_reconhecimentos = 0
+
+        for foto in fotos_individuo:
+            foto = reconhecedor.load_image_file(foto)
+            caracteristicas = reconhecedor.face_encodings(foto)
+
+            if not caracteristicas:
+                continue
+
+            reconhecimentos = reconhecedor.compare_faces(
+                caracteristicas_visitante, caracteristicas)
+            if True in reconhecimentos:
+                total_de_reconhecimentos += 1
+
+        confianca = total_de_reconhecimentos / len(fotos_individuo) if len(fotos_individuo) > 0 else 0
+        reconhecido = confianca >= 0.6
+
+        return reconhecido, confianca
 
 
 if __name__ == '__main__':
     GATE = GateSecuritySystem()
     GATE.load_config()
-
