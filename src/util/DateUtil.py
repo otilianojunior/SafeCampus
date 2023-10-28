abae12f156ae8fea6201c021b757d8c22900d412
Otiliano Junior <otiliano.rodrigues@veri.com.br>
2023-10-28 16:42:44 -0300

    feat: atualização da classe dateUtil

--- src/util/DateUtil.py
+++ src/util/DateUtil.py
@@ -1,16 +1,18 @@
 from datetime import datetime, time, timedelta
 import random
 
+
 class DateUtil:
     def gerar_horario_entrada(self):
-        intervalos = [
-            (time(8, 0), time(12, 0)),
-            (time(13, 30), time(18, 0)),
-            (time(18, 30), time(22, 0))
-        ]
+        turnos = {
+            "manha": (time(8, 0), time(12, 0)),
+            "tarde": (time(13, 0), time(18, 0)),
+            "noite": (time(18, 30), time(22, 0))
+        }
 
-        # Escolha um intervalo aleatório
-        intervalo_escolhido = random.choice(intervalos)
+        # Escolha um turno aleatório
+        turno_escolhido = random.choice(list(turnos.keys()))
+        intervalo_escolhido = turnos[turno_escolhido]
 
         # Gere uma hora aleatória dentro do intervalo escolhido
         hora_entrada = datetime.combine(datetime.today(), intervalo_escolhido[0])
@@ -20,17 +22,18 @@ class DateUtil:
         return hora_entrada
 
     def gerar_horario_saida(self, hora_entrada):
-        # Defina o limite máximo de saída como 22:00
+        # Obtenha o limite máximo de saída como 22:00
         limite_maximo_saida = time(22, 0)
 
-        while True:
-            hora_saida = datetime.combine(datetime.today(), limite_maximo_saida)
-            # Adicione um valor aleatório entre 60 e 240 minutos (1 a 4 horas)
-            minutos_aleatorios = random.randint(60, 240)
-            hora_saida += timedelta(minutes=minutos_aleatorios)
+        # Gere um horário de saída aleatório que seja após o horário de entrada
+        hora_saida = datetime.combine(datetime.today(), limite_maximo_saida)
 
-            if hora_entrada < hora_saida:
-                return hora_saida
+        while hora_saida <= hora_entrada or hora_saida.hour >= 22:
+            minutos_aleatorios = random.randint(0, (limite_maximo_saida.hour - hora_entrada.time().hour) * 60)
+            hora_saida = datetime.combine(datetime.today(), hora_entrada.time()) + timedelta(minutes=minutos_aleatorios)
+
+        return hora_saida
+
 
 # Exemplo de uso:
 date_util = DateUtil()
