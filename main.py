7472460172d9ac570673b46611c5e557c68c5bd3
Otiliano Junior <otiliano.rodrigues@veri.com.br>
2023-11-11 15:42:38 -0300

    feat: reomoção de print

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     preparado, configuracao = safe_campus.load_config()
     fotos_portao = safe_campus.load_fotos(config.DIR_FOTOS_GATE)
     foto_entrada = safe_campus.simular_entrada(fotos_portao)
-    print(foto_entrada)
 
     alunos = safe_campus.load_fotos(config.DIR_FOTOS_ALUNOS)
     suspeitos = safe_campus.load_fotos(config.DIR_FOTOS_SUSPEITOS)
@@ -19,14 +18,14 @@ if __name__ == "__main__":
 
     safe_campus.PROBABILIDADE_SAIDA = 30
     safe_campus.TEMPO_MEDIO_PERMANENCIA = 80
-    safe_campus.TEMPO_LIBERACAO_INDIVIDUOS = 60
+    safe_campus.TEMPO_LIBERACAO_INDIVIDUOS = 30
     safe_campus.TEMPO_DETECCAO_INDIVIDUOS = 40
 
     ambiente_de_simulacao = simpy.Environment()
-    ambiente_de_simulacao.process(safe_campus.reconhecer_visitantes(ambiente_de_simulacao, foto_entrada, configuracao))
-    ambiente_de_simulacao.process(safe_campus.reconhecer_alunos(ambiente_de_simulacao, foto_entrada, configuracao))
-    ambiente_de_simulacao.process(safe_campus.reconhecer_suspeitos(ambiente_de_simulacao, foto_entrada, configuracao))
-    ambiente_de_simulacao.process(safe_campus.reconhecer_professores(ambiente_de_simulacao, foto_entrada, configuracao))
+    ambiente_de_simulacao.process(safe_campus.reconhecer_visitantes(ambiente_de_simulacao, foto_entrada, configuracao, fotos_portao))
+    ambiente_de_simulacao.process(safe_campus.reconhecer_alunos(ambiente_de_simulacao, foto_entrada, configuracao, fotos_portao))
+    ambiente_de_simulacao.process(safe_campus.reconhecer_suspeitos(ambiente_de_simulacao, foto_entrada, configuracao, fotos_portao))
+    ambiente_de_simulacao.process(safe_campus.reconhecer_professores(ambiente_de_simulacao, foto_entrada, configuracao, fotos_portao))
 
     ambiente_de_simulacao.process(safe_campus.simula_saida(ambiente_de_simulacao))
     ambiente_de_simulacao.run(until=2000)
