e2d9a2360734627967dec32afc031e397a430dce
Otiliano Junior <otiliano.rodrigues@veri.com.br>
2023-11-15 18:44:57 -0300

    feat: atualização behave concluida

--- registrar_saida.feature
+++ registrar_saida.feature
@@ -4,5 +4,5 @@
   Scenario: É salvo os hoários de entrada e saida de um indivíduo.
 
   Given o ambiente de registro esteja preparado
-  When e necessario reconhecer a foto src/assets/fotos/gate/foto_portao.jpeg de algum individuo para que ele saia e registre os horarios
-  Then o individuo que entrou na instituicao e saiu e registrado o horario
+  When e necessario que alguem entre e reconheca a foto src/assets/fotos/gate/foto_portao.jpeg para que ele saia e registre os horarios
+  Then o individuo que entrou na instituicao saiu e foram registrados o horarios
--- steps/registrar_saida.py
+++ steps/registrar_saida.py
@@ -10,7 +10,7 @@ def given_ambiente_registro_preparado(contexto):
     assert preparado
 
 
-@When('e necessario reconhecer a foto {foto} de algum individuo para que ele saia e registre os horarios')
+@When('e necessario que alguem entre e reconheca a foto {foto} para que ele saia e registre os horarios')
 def when_foto_capturada_registro(contexto, foto):
     contexto.foto_entrada = SafeCampus().simular_entrada([foto])
     contexto.alunos = SafeCampus().load_fotos(Configuration().DIR_FOTOS_ALUNOS)
@@ -18,7 +18,7 @@ def when_foto_capturada_registro(contexto, foto):
     assert contexto.foto_entrada is not None
 
 
-@Then('o individuo que entrou na instituicao e saiu e registrado o horario')
+@Then('o individuo que entrou na instituicao saiu e foram registrados o horarios')
 def the_registra_horario(contexto):
     ocorreram_reconhecimentos, contexto.alunos_reconhecidos, contexto.individuos_registrados = SafeCampus().reconhecer_alunos(contexto.foto_entrada, contexto.configuracao)
     PROBABILIDADE_SAIDA = 100
